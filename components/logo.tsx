d4d20caad8593f1ab294848fcee4c6fed192804b
fisayo-dev <olufisayobadina@gmail.com>
2025-02-18 22:26:15 +0100

    bolded sidebar title

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@
 const logo = () => {
   return (
     <div className="flex py-2 items-center justify-between">
-    <h1 className="text-2xl font-semibold">BookMark Hub</h1>
+    <h1 className="text-2xl font-extrabold">BookMark Hub</h1>
 </div>
   )
 }
