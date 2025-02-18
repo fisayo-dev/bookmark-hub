828bfee6637676e78622352d811489a823cf3100
fisayo-dev <olufisayobadina@gmail.com>
2025-02-18 21:14:44 +0100

    create logo component

---
+++ components/logo.tsx
@@ -0,0 +1,9 @@
+const logo = () => {
+  return (
+    <div className="flex items-center justify-between">
+    <h1 className="text-2xl font-semibold">BookMark Hub</h1>
+</div>
+  )
+}
+
+export default logo
