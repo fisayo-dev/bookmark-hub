1572740fec94c2ec3636a6c59bfb9a5f7bc3c038
fisayo-dev <olufisayobadina@gmail.com>
2025-02-18 21:15:07 +0100

    change link sidebar color to shite

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,7 @@
+"use client"
 const logo = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex py-2 items-center justify-between">
     <h1 className="text-2xl font-semibold">BookMark Hub</h1>
 </div>
   )
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,9 +2,9 @@
 import BookmarkHub from '@/components/logo'
 const sidebar = () => {
   return (
-      <div className="h-[100vh] bg-blue-500 container w-[25vw]">
-          <div className="py-4 px-6 mx-auto">
-               <BookmarkHub/>
+      <div className="h-[100vh]  bg-blue-500 container w-[25vw]">
+          <div className="text-white py-4 px-6 mx-auto">
+              <BookmarkHub />
               <div className="flex flex-col justify-between h-full">
                   <p>dd</p>
                   <p>dd</p>
