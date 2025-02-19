d4a4d40ba975a912b6b52289939bf097c63a2db3
fisayo-dev <olufisayobadina@gmail.com>
2025-02-19 16:48:14 +0100

    restyling bookmarks hub

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 import BookmarkHub from "@/components/logo";
 import { ReactNode } from "react";
 
-const Bookmark = dynamic(() => import("iconsax-react").then((mod) => mod.Bookmark), { ssr: false });
+const Bookmark = dynamic(() => import("iconsax-react").then((mod) => mod.Home), { ssr: false });
 const Profile = dynamic(() => import("iconsax-react").then((mod) => mod.Profile), { ssr: false });
 const Create = dynamic(() => import("iconsax-react").then((mod) => mod.Add), { ssr: false });
 const Star1 = dynamic(() => import("iconsax-react").then((mod) => mod.Star1), { ssr: false });
@@ -21,16 +21,16 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   const links: SidebarProps[] = [
-    { link: "/bookmarks", text: "Bookmarks", icon: (color) => <Bookmark size="24" color={color} variant="Bold" /> },
-    { link: "/create-bookmark", text: "Create", icon: (color) => <Create size="24" color={color} variant="Bold" /> },
-    { link: "/favorites", text: "Favorites", icon: (color) => <Star1 size="24" color={color} variant="Bold" /> },
-    { link: "/account", text: "Account", icon: (color) => <Profile size="24" color={color} variant="Bold" /> },
+    { link: "/bookmarks", text: "Bookmarks", icon: (color) => <Bookmark size="24" color={color}  /> },
+    { link: "/create-bookmark", text: "Create", icon: (color) => <Create size="24" color={color}  /> },
+    { link: "/favorites", text: "Favorites", icon: (color) => <Star1 size="24" color={color}  /> },
+    { link: "/account", text: "Account", icon: (color) => <Profile size="24" color={color}  /> },
   ];
 
   return (
     <div className="h-[100vh] w-[25vw] bg-white shadow">
           <div className="grid gap-6 py-4 px-6 mx-auto">
-              <div className="px-3 py-4">
+              <div className="py-4">
                   <BookmarkHub />
               </div>
         <div className="flex gap-2 flex-col justify-between h-full">
@@ -41,11 +41,11 @@ const Sidebar = () => {
                 href={item.link}
                 key={index}
                 className={`flex px-3 py-4 rounded-2xl items-center space-x-2 transition-all duration-300 ${
-                  isActive ? "bg-gray-200 font-bold" : "hover:bg-gray-100"
+                  isActive ? "bg-gray-100 font-bold" : "hover:bg-gray-50"
                 }`}
               >
                 {item.icon(isActive ? "black" : "#aaaaaa")} 
-                <span>{item.text}</span>
+                <span className={isActive ? "text-black" : "text-gray-500"}>{item.text}</span>
               </Link>
             );
           })}
