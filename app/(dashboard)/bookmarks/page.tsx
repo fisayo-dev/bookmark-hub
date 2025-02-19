ab3339719b33d8cb8de7c21d5e5a4d950f5c5570
fisayo-dev <olufisayobadina@gmail.com>
2025-02-19 17:13:57 +0100

    wrap rest content in container

--- app/(dashboard)/bookmarks/page.tsx
+++ app/(dashboard)/bookmarks/page.tsx
@@ -4,8 +4,12 @@ const pages = () => {
     <div className="my-10">
       <div className="grid gap-4">
         <h2 className="text-4xl text-center color-pink font-bold">My Bookmarks</h2>
-        <div className="flex-items-center gap-4">
-          <div className="bg-gray-200 p-4 rounded-2xl"></div>
+        <div className="flex-items-center gap-4 app-container">
+          <div className="bg-gray-200 p-4 rounded-2xl">
+            <div className="flex items-center">
+
+            </div>
+          </div>
           
         </div>
       </div>
