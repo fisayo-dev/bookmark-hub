525c52a931857bc38d5c731a81c18e1986efd1d0
fisayo-dev <olufisayobadina@gmail.com>
2025-02-19 20:28:02 +0100

    edited loading ui display

--- app/(dashboard)/bookmarks/loading.tsx
+++ app/(dashboard)/bookmarks/loading.tsx
@@ -1,13 +1,13 @@
 const Loading = () => {
     return (
-      <div className="space-y-4 p-4">
+      <div className="space-y-6 p-4 mt-10">
         {/* Skeleton Header */}
-        <div className="h-6 w-48 bg-gray-200 rounded-md animate-pulse"></div>
+        <div className="h-10 w-64 mx-auto bg-gray-200 rounded-md animate-pulse"></div>
         
         {/* Skeleton Cards */}
         <div className="space-y-2">
           {[...Array(3)].map((_, index) => (
-            <div key={index} className="h-16 w-full bg-gray-300 rounded-md animate-pulse"></div>
+            <div key={index} className="h-16 w-3/4 mx-auto bg-gray-200 rounded-md animate-pulse"></div>
           ))}
         </div>
       </div>
