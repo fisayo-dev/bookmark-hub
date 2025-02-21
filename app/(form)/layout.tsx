53aae87f567d66b2a584b975e95a239386a4d360
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 20:58:11 +0100

    redesigning auth layout

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -1,17 +1,17 @@
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="grid md:grid-cols-2">
-      <div className="bg-pink text-white h-[100vh]">
-        <div className="flex flex-col h-[100vh] items-center place-content-center justify-items-center">
+    <div className="grid w-full h-[100vh] md:grid-cols-2 p-2 bg-gray-200">
+      <div className="bg-pink h-full text-white rounded-3xl overflow-hidden">
+        <div className="flex flex-col items-center place-content-center justify-items-center">
           <div className="grid items-center text-center">
             <h2 className="text-3xl font-bold">Bookmark Hub</h2>
             <p>Your all in one bookmark store. <br /> Organize your bookmarks in one place.</p>
           </div>
         </div>
       </div>
-      <div className="bg-gray-200 h-[100vh] p-4">
+      <div className="p-4 h-full">
         {children}
-      </div>
+      </div> 
     </div>
   )
 }
