3b7c78750d730d08e8685a4f23cce6a4d047cedc
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 20:42:39 +0100

    creating auth layout

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -1,6 +1,18 @@
-const layout = () => {
+const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>layout</div>
+    <div className="grid md:grid-cols-2">
+      <div className="bg-pink text-white h-[100vh]">
+        <div className="flex flex-col h-[100vh] items-center place-content-center justify-items-center">
+          <div className="grid items-center text-center">
+            <h2 className="text-3xl font-bold">Bookmark Hub</h2>
+            <p>Your all in one bookmark store. <br /> Organize your bookmarks in one place.</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-200 h-[100vh]">
+        {children}
+      </div>
+    </div>
   )
 }
 
