fb744724ce7b75492c1f650ee4ddb18f5d5e1d45
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 21:02:18 +0100

    designing layout

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -1,18 +1,29 @@
+import Image from "next/image"
+
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="grid w-full h-[100vh] md:grid-cols-2 p-2 bg-gray-200">
-      <div className="bg-pink h-full text-white rounded-3xl overflow-hidden">
-        <div className="flex flex-col items-center place-content-center justify-items-center">
-          <div className="grid items-center text-center">
+      <div className="bg-pink text-white rounded-3xl overflow-hidden">
+        <div className="flex h-full flex-col items-center place-content-center justify-items-center">
+          {/* <div className="grid items-center text-center">
             <h2 className="text-3xl font-bold">Bookmark Hub</h2>
             <p>Your all in one bookmark store. <br /> Organize your bookmarks in one place.</p>
-          </div>
+          </div> */}
+          {/* <Image
+            height={100}
+            width={100}
+            src='/bookmark.svg'
+            alt="Bookmark Hub"
+            className="w-full"
+          /> */}
         </div>
       </div>
       <div className="p-4 h-full">
-        {children}
+        <div className="flex h-full flex-col items-center place-content-center justify-items-center">
+          {children}
+         </div>
       </div> 
-    </div>
+    </div> 
   )
 }
 
