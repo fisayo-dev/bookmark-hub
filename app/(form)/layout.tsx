c61a0b9b376e36a25a76a2da6c044c9461b63dc1
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 22:56:45 +0100

    remove border

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="grid w-full h-[100vh] md:grid-cols-2 bg-white">
-      <div className=" text-white">
+      <div className="hidden md:block text-white">
         <div className="flex h-full flex-col items-center place-content-center justify-items-center">
           {/* <div className="grid items-center text-center">
             <h2 className="text-3xl font-bold">Bookmark Hub</h2>
@@ -12,7 +12,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
           {/* <x` */}
         </div>
       </div>
-      <div className="p-4 h-full border-l-2 border-l-gray-200">
+      <div className="p-4 h-full">
         <div className="flex h-full flex-col items-center place-content-center justify-items-center">
           {children}
          </div>
