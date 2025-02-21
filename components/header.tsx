b923d0c56761c9fcd80ca9e8b5af705622f77664
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 20:33:12 +0100

    optimized link display

--- components/header.tsx
+++ components/header.tsx
@@ -2,14 +2,34 @@ import { Menu } from "iconsax-react"
 import Link from "next/link"
 import BookmarkHub from '@/components/logo'
 
+interface Links  {
+    name: string,
+    url: string,
+}
+
 const header = () => {
+    const links: Links[] = [
+        {
+            name: 'Contact',
+            url: '/contact',
+        },
+        {
+            name: 'Signup',
+            url: '/signup',
+        },            
+        {
+            name: 'Login',
+            url: '/login',
+        }            
+    ]
   return (
     <div className="app-container py-6">
           <div className="flex items-center justify-between">
               <BookmarkHub />
               <div className="hidden md:flex items-center gap-6">
-                  <Link href='/contact' className="border-b-transparent border-b-[0.15rem] hover:border-b-gray-900">Contact</Link>
-                  <Link href="/developer" className="border-b-transparent border-b-[0.15rem] hover:border-b-gray-900">Developer</Link>
+                  {links.map((link, index) => (
+                        <Link key={index} href={link.url} className="border-b-transparent border-b-[0.15rem] hover:border-b-gray-900">{link.name}</Link>
+               ))}
               </div>
               <div className="flex md:hidden">
                   <Menu className="h-6 w-6" />
