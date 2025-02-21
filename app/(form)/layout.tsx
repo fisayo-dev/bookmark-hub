a8173358d5d9856b14e87285696e40845b48c582
fisayo-dev <olufisayobadina@gmail.com>
2025-02-21 22:47:40 +0100

    designed signup form elements

--- app/(form)/layout.tsx
+++ app/(form)/layout.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="grid w-full h-[100vh] md:grid-cols-2 bg-gray-200">
+    <div className="grid w-full h-[100vh] md:grid-cols-2 bg-white">
       <div className="bg-pink text-white">
         <div className="flex h-full flex-col items-center place-content-center justify-items-center">
           {/* <div className="grid items-center text-center">
--- app/(form)/signup/page.tsx
+++ app/(form)/signup/page.tsx
@@ -1,6 +1,6 @@
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Lock } from "iconsax-react"
-import { Eye, Key, LockIcon, Mail, User } from "lucide-react"
+import { Eye, Key, Mail, User } from "lucide-react"
 import Link from "next/link"
 
 const page = () => {
@@ -11,44 +11,47 @@ const page = () => {
         <div className="flex items-center gap-1 justify-center">
           <p>Already have an account</p>
           <Link href="/login" className="color-pink hover:underline ">Login</Link>
-          </div>
+        </div>
       </div>
-      <div className="grid gap-2">
-        <div className="grid gap-2 grid-cols-2">
-          <div className="p-3 border-1 rounded-md bg-white shadow-sm">
+      <div className="grid gap-3">
+        <div className="grid gap-3 grid-cols-2">
+          <div className="p-3 border-1 rounded-2xl bg-gray-100 shadow-sm">
             <div className="flex items-center">
               <User className="text-gray-700"/>
               <Input placeholder="First Name" type="text"/>
             </div>
           </div>
-          <div className="p-3 border-1 rounded-md bg-white shadow-sm">
+          <div className="p-3 border-1 rounded-2xl bg-gray-100 shadow-sm">
             <div className="flex items-center">
               <User className="text-gray-700"/>
               <Input placeholder="Last Name" type="text"/>
             </div>
           </div>
         </div>
-        <div className="p-3 border-1 rounded-md bg-white shadow-sm">
+        <div className="p-3 border-1 rounded-2xl bg-gray-100 shadow-sm">
             <div className="flex items-center">
               <Mail className="text-gray-700"/>
               <Input placeholder="Email Address" type="email"/>
             </div>
         </div>
-        <div className="p-3 border-1 rounded-md bg-white shadow-sm">
+        <div className="p-3 border-1 rounded-2xl bg-gray-100 shadow-sm">
             <div className="flex items-center">
               <Key className="text-gray-700"/>
               <Input placeholder="Your password" type="password" />
               <Eye />
             </div>
         </div>
-        <div className="p-3 border-1 rounded-md bg-white shadow-sm">
+        <div className="p-3 border-1 rounded-2xl bg-gray-100 shadow-sm">
             <div className="flex items-center">
               <Key className="text-gray-700"/>
               <Input placeholder="Repeat password" type="password"/>
                 <Eye />
             </div>
-          </div>
+        </div>
       </div>
+        <Button className="flex items-center gap-2">
+          <p>Submit</p>
+        </Button>
     </div>
   )
 }
