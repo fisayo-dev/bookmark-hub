5d750ad1cd0ad18a33359573aa26911637133ea7
fisayo-dev <olufisayobadina@gmail.com>
2025-02-27 00:38:58 +0100

    added btn actions between dashboard and create-bookmark routes in respective pages

--- app/(dashboard)/bookmarks/page.tsx
+++ app/(dashboard)/bookmarks/page.tsx
@@ -1,4 +1,6 @@
 import BookmarkLists from "@/components/dashboard/BookmarkLists";
+import {BookmarkIcon} from 'lucide-react'
+import Link from "next/link";
 
 interface Bookmark {
   userId: number,
@@ -20,7 +22,15 @@ export default async function Pages() {
   return (
     <div className="my-10">
       <div className="grid gap-4">
-        <h2 className="app-container text-4xl color-pink font-bold">Bookmarks</h2>
+        <div className='app-container flex items-center justify-between'>
+          <h2 className=" text-4xl color-pink font-bold">Bookmarks</h2>
+          <div className='p-3 rounded-2xl bg-gray-100 hover:bg-gray-200 cursor-pointer'>
+            <Link href="/create-bookmark" className='flex items-center justify-center gap-1'>
+              <BookmarkIcon className='h-5 w-5'/>
+              <p className='text-sm'>New</p>
+            </Link>
+          </div>
+        </div>
         <BookmarkLists bookmarks={bookmarks} />
       </div>
     </div>
--- app/(dashboard)/create-bookmark/page.tsx
+++ app/(dashboard)/create-bookmark/page.tsx
@@ -1,29 +1,38 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Bookmark, PlusCircle } from 'lucide-react'
+import {Bookmark, BookmarkIcon, PlusCircle} from 'lucide-react'
 import React from 'react'
+import Link from "next/link";
 
 const page = () => {
   return (
     <div className="my-10">
       <div className="grid gap-4">
-        <h2 className="text-4xl color-pink font-bold app-container">Create</h2>
+        <div className='app-container flex items-center justify-between'>
+          <h2 className="text-4xl color-pink font-bold ">Create</h2>
+          <div className='p-3 rounded-2xl bg-gray-100 hover:bg-gray-200 cursor-pointer'>
+            <Link href="/bookmarks" className='flex items-center justify-center gap-1'>
+              <p className='text-sm'>Go to bookmarks</p>
+            </Link>
+          </div>
+        </div>
         <div className="app-container">
           <div className="flex items-center py-4 gap-4">
             <div className="bg-gray-100 w-full px-4 py-3 rounded-2xl">
               <div className="flex items-center gap-2">
                 <Bookmark className='h-6 w-6 text-gray-400'/>
-                <Input className='w-full px-0 py-1' placeholder='Type in the link of the web page you want to bookmark'/>
+                <Input className='w-full px-0 py-1'
+                       placeholder='Type in the link of the web page you want to bookmark'/>
               </div>
             </div>
-          <Button className='flex items-center'>
-            <PlusCircle className='h-8 w-8'/>
-            <p>Create</p>
-          </Button>
+            <Button className='flex items-center'>
+              <PlusCircle className='h-8 w-8'/>
+              <p>Create</p>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
