19bb84aa93669bfbf6d20927180d72e86c97f1a8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-01 22:05:30 +0100

    update schema field name

--- database/schema.ts
+++ database/schema.ts
@@ -5,7 +5,7 @@ export const users = pgTable("users", {
     id: uuid('id').notNull().primaryKey().defaultRandom().unique(),
     fullName: varchar('full_name', { length: 255 }).notNull(),
     email: text('email').notNull().unique(),
-    passwords: text('password').notNull(),
+    password: text('password').notNull(),
     profile_image_url: text('profile_image_url').notNull(),
     subscription: SUBSCRIPTION_ENUM('subscription').default('FREE'),
     lastActivityDate: date('last_activity_date').notNull().defaultNow(),
