ac440290fb43e838306595c38829ccae248c85b8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-04 14:31:56 +0100

    added func to toggle showing sidebar element

--- components/ResponsiveHeader.tsx
+++ components/ResponsiveHeader.tsx
@@ -1,21 +1,29 @@
+"use client"
 import Logo from "@/components/logo";
 import {MenuIcon} from "lucide-react";
 import {SidebarContent} from "@/components/sidebar";
+import {useEffect, useState} from "react";
 
 const ResponsiveHeader = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <div>
-            <div className="block lg:hidden absolute top-0 left-0 z-50">
-                <div className="h-[100vh] w-[50vw] md:w-[35vw] lg:w-[25vw] bg-white shadow">
-                    <SidebarContent />
+            {showMenu && (
+                <div>
+                    <div className="block lg:hidden absolute top-0 left-0 z-50">
+                        <div className="h-[100vh] w-[50vw] md:w-[35vw] lg:w-[25vw] bg-white shadow">
+                            <SidebarContent />
+                        </div>
+                    </div>
+                    <div onClick={() => setShowMenu(false)} className="absolute w-[100vw] h-[100vh] top-0 left-0 bg-black/25 z-40"></div>
                 </div>
-            </div>
+            )}
             <div className="bg-transparent backdrop-blur-md lg:hidden fixed w-full">
                 <div className="app-container py-2 flex items-center justify-between">
                     <div>
                         <Logo/>
                     </div>
-                    <div className='p-2 cursor-pointer hover:bg-gray-100 rounded-full'>
+                    <div onClick={() => setShowMenu(true)} className='p-2 cursor-pointer hover:bg-gray-100 rounded-full'>
                         <MenuIcon />
                     </div>
                 </div>
