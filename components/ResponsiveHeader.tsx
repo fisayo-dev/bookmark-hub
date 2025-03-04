0cf2ad5e34bd95a954654b5abeaaa7fdc091683a
fisayo-dev <olufisayobadina@gmail.com>
2025-03-04 14:13:17 +0100

    detach sidebar component into seperate component

--- components/ResponsiveHeader.tsx
+++ components/ResponsiveHeader.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
 import Logo from "@/components/logo";
 import {MenuIcon} from "lucide-react";
 
 const ResponsiveHeader = () => {
     return (
-        <div className="bg-transparent backdrop-blur-md lg:hidden fixed w-full">
-            <div className="app-container py-2 flex items-center justify-between">
-                <div>
-                    <Logo/>
-                </div>
-                <div className='p-2 cursor-pointer hover:bg-gray-100 rounded-full'>
-                    <MenuIcon />
+        <div>
+            <div className="absolute">
+
+            </div>
+            <div className="bg-transparent backdrop-blur-md lg:hidden fixed w-full">
+                <div className="app-container py-2 flex items-center justify-between">
+                    <div>
+                        <Logo/>
+                    </div>
+                    <div className='p-2 cursor-pointer hover:bg-gray-100 rounded-full'>
+                        <MenuIcon />
+                    </div>
                 </div>
             </div>
         </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,43 +14,50 @@ interface SidebarProps {
   icon: (color: string) => ReactNode;
 }
 
-const Sidebar = () => {
-  const pathname = usePathname();
-
-  const links: SidebarProps[] = [
+const links: SidebarProps[] = [
     { link: "/bookmarks", text: "Bookmarks", icon: (color) => <BookmarkIcon size="24" color={color}  /> },
     { link: "/create-bookmark", text: "Create", icon: (color) => <PlusIcon size="24" color={color}  /> },
     { link: "/favorites", text: "Favorites", icon: (color) => <StarIcon size="24" color={color}  /> },
     { link: "/subscriptions", text: "Subscriptions", icon: (color) => <WalletIcon size="24" color={color}  /> },
     { link: "/account", text: "Account", icon: (color) => <ProfileCircle size="24" color={color}  /> },
-  ];
+];
 
-  return (
-    <div className="hidden lg:block h-[100vh] w-[21vw] bg-white shadow">
-          <div className="grid gap-6 py-4 px-6 mx-auto">
-              <div className="py-4">
-                  <BookmarkHub />
-              </div>
-        <div className="flex gap-2 flex-col justify-between h-full">
-          {links.map((item, index) => {
-            const isActive = pathname === item.link;
-            return (
-              <Link
-                href={item.link}
-                key={index}
-                className={`flex px-3 py-4 rounded-2xl items-center space-x-2 transition-all duration-300 ${
-                  isActive ? "bg-gray-100 font-bold" : "hover:bg-gray-50"
-                }`}
-              >
-                {item.icon(isActive ? "black" : "#aaaaaa")} 
-                <span className={isActive ? "text-black" : "text-gray-500"}>{item.text}</span>
-              </Link>
-            );
-          })}
+export const SidebarContent = () =>  {
+    const pathname = usePathname();
+
+    return (
+        <div className="grid gap-6 py-4 px-6 mx-auto">
+            <div className="py-4">
+                <BookmarkHub/>
+            </div>
+            <div className="flex gap-2 flex-col justify-between h-full">
+                {links.map((item, index) => {
+                    const isActive = pathname === item.link;
+                    return (
+                        <Link
+                            href={item.link}
+                            key={index}
+                            className={`flex px-3 py-4 rounded-2xl items-center space-x-2 transition-all duration-300 ${
+                                isActive ? "bg-gray-100 font-bold" : "hover:bg-gray-50"
+                            }`}
+                        >
+                            {item.icon(isActive ? "black" : "#aaaaaa")}
+                            <span className={isActive ? "text-black" : "text-gray-500"}>{item.text}</span>
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
+}
+
+
+const Sidebar = () => {
+    return (
+        <div className="hidden lg:block h-[100vh] w-[21vw] bg-white shadow">
+            <SidebarContent />
+        </div>
+    );
 };
 
 export default Sidebar;
