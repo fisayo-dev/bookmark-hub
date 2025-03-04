14c03c4a1be365b6cd3d7434e41cb81f169aa384
fisayo-dev <olufisayobadina@gmail.com>
2025-03-04 14:18:05 +0100

    creating repsonsive sidebar

--- components/ResponsiveHeader.tsx
+++ components/ResponsiveHeader.tsx
@@ -1,11 +1,14 @@
 import Logo from "@/components/logo";
 import {MenuIcon} from "lucide-react";
+import {SidebarContent} from "@/components/sidebar";
 
 const ResponsiveHeader = () => {
     return (
         <div>
-            <div className="absolute">
-
+            <div className="block lg:hidden absolute top-0 left-0 z-50">
+                <div className="h-[100vh] w-[50vw] md:w-[35vw] lg:w-[25vw] bg-white shadow">
+                    <SidebarContent />
+                </div>
             </div>
             <div className="bg-transparent backdrop-blur-md lg:hidden fixed w-full">
                 <div className="app-container py-2 flex items-center justify-between">
