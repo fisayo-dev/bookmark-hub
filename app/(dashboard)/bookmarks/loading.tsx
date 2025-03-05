1895aef071a115c8d0a8755f3b89c031f18f04f8
fisayo-dev <olufisayobadina@gmail.com>
2025-03-05 01:26:00 +0100

    made loading elements repsonsive

--- app/(dashboard)/bookmarks/loading.tsx
+++ app/(dashboard)/bookmarks/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
         <div className="h-14 w-9/12 mx-auto bg-gray-200 rounded-md animate-pulse"></div>
         
         {/* Skeleton Cards */}
-        <div className="grid gap-2 grid-cols-4 app-container">
+        <div className="grid gap-2 grid-cols-2 md:grid-cols-4 app-container">
           {[...Array(4)].map((_, index) => (
             <div key={index} className="h-[35vh] bg-gray-200 rounded-2xl animate-pulse"></div>
           ))}
