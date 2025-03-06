2cc5143208d7b47006042d383a84069dbf4cdf04
fisayo-dev <olufisayobadina@gmail.com>
2025-03-06 13:43:10 +0100

    place session provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header"
 import { ReactNode } from "react"
 import {auth} from "@/auth";
 import {redirect} from "next/navigation";
+import {SessionProvider} from "next-auth/react";
 
 interface LayoutProps {
     children: ReactNode
@@ -9,12 +10,15 @@ interface LayoutProps {
 const layout = async ({children}: LayoutProps) => {
     const session = await auth()
     if (session) redirect('/bookmarks')
+
     return (
       <div>
-            <Header />
-            <div className="app-container py-6">
-                {children}
-            </div>
+          <SessionProvider  session={session}>
+                <Header />
+                <div className="app-container py-6">
+                    {children}
+                </div>
+          </SessionProvider>
     </div>
   )
 }
