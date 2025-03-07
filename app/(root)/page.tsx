301df7ca7385826bee7d1b201fa4dd326ac0c8d7
fisayo-dev <olufisayobadina@gmail.com>
2025-03-08 00:37:59 +0100

    remove unused import

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
-import dynamic from "next/dynamic";
 import {RocketIcon, SearchIcon} from "lucide-react";
 
 export default function Home() {
