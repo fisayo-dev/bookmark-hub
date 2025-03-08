276d273e251e0d2f37706d9f1ac3181c9603780e
fisayo-dev <olufisayobadina@gmail.com>
2025-03-08 20:46:49 +0100

    chore: code link to open in new tab

--- components/dashboard/BookmarkCard.tsx
+++ components/dashboard/BookmarkCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const BookmarkCard = ({view, name, url }: {name: string; view: string; url: string }) => {
+const BookmarkCard = ({ view, name, url }: { name: string; view: string; url: string }) => {
     return (
         <div
             className={`hover:border-gray-400 overflow-hidden cursor-pointer border border-gray-200 rounded-2xl p-2 ${
@@ -11,19 +11,28 @@ const BookmarkCard = ({view, name, url }: {name: string; view: string; url: stri
             <Image
                 height={100}
                 width={100}
-                className={`${view == 'grid' ? 'mx-auto' :
-                    'w-24'}`}
+                className={`${view === 'grid' ? 'mx-auto' : 'w-24'}`}
                 src="/not-found.svg"
                 alt={name}
             />
             <div className="px-4">
-                <h2 className="text-nowrap capitalize text-2xl font-bold">{name.length > 12 ? `${name.substring(0, 12)}...` : name}</h2>
-                <p className="text-nowrap text-sm">{url.length > 30 ? `${url.substring(0, 30)}...` : url}</p>
-                <Link href={url} className="text-sm hover:underline">
+                <h2 className="text-nowrap capitalize text-2xl font-bold">
+                    {name.length > 12 ? `${name.substring(0, 12)}...` : name}
+                </h2>
+                <p className="text-nowrap text-sm">
+                    {url.length > 30 ? `${url.substring(0, 30)}...` : url}
+                </p>
+                <Link
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm hover:underline"
+                >
                     Visit
                 </Link>
             </div>
         </div>
-    )
-}
-export default BookmarkCard
+    );
+};
+
+export default BookmarkCard;
