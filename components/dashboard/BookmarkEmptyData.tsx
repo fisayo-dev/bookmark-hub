4f085672c7408b9f52dced8ae2b5684e6667a1a0
fisayo-dev <olufisayobadina@gmail.com>
2025-03-11 00:29:39 +0100

    chore: modified bookmark no data component to accept link (optional)

--- components/dashboard/BookmarkEmptyData.tsx
+++ components/dashboard/BookmarkEmptyData.tsx
@@ -9,9 +9,10 @@ interface Props {
     image_url: string;
     btn_text: string;
     image_alt_msg?: string;
+    url_link?: string;
 }
 
-const BookmarkData = ({text, subtext, image_url, btn_text, image_alt_msg}: Props) => {
+const BookmarkData = ({text, subtext, image_url, btn_text, image_alt_msg, url_link}: Props) => {
     return (
         <div className="app-container mt-20 text-gray-900">
             <div className="grid gap-2 justify-center">
@@ -24,7 +25,7 @@ const BookmarkData = ({text, subtext, image_url, btn_text, image_alt_msg}: Props
                 />
                 <h2 className="mx-auto text-2xl font-bold text-center">{text}</h2>
                 {subtext && (<p className="mx-auto text-sm text-center">{subtext}</p>)}
-                <Link href="/create-bookmark" className="mx-auto">
+                <Link href={url_link || "/create-bookmark"} className="mx-auto">
                     <Button className="text-center flex gap-2 justify-between items-center">
                         <PlusIcon/>
                         <p>{btn_text}</p>
